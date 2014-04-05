8b25326cde488654e9be5b94b32539a129acd1ef
Andrew Dorsett <awdorsett@berkeley.edu>
2014-04-05 15:14:10 -0700

    added edit fields

---
+++ src/com/example/statthat/GameSetup.java
@@ -0,0 +1,22 @@
+package com.example.statthat;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GameSetup extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_setup);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.game_setup, menu);
+		return true;
+	}
+
+}
