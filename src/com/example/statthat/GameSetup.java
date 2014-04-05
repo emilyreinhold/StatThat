99307659c734e3510c3b43b99a2388898478a20e
Andrew Dorsett <awdorsett@berkeley.edu>
2014-04-05 16:17:20 -0700

    basics of layout are done, some functions, min SDK => 9

--- src/com/example/statthat/GameSetup.java
+++ src/com/example/statthat/GameSetup.java
@@ -1,8 +1,12 @@
 package com.example.statthat;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class GameSetup extends Activity {
 
@@ -10,6 +14,18 @@ public class GameSetup extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_setup);
+		
+		//Submit form
+		Button confirm = (Button) findViewById(R.id.confirm);
+		confirm.setOnClickListener(new Button.OnClickListener(){  
+
+			@Override
+			public void onClick(View arg0) {
+				if(validateFields()){
+					
+				}
+			};
+         });
 	}
 
 	@Override
@@ -17,6 +33,16 @@ public class GameSetup extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.game_setup, menu);
 		return true;
+	}
+	
+	// Check to see if required fields are populated
+	private boolean validateFields(){
+		EditText location = (EditText) findViewById(R.id.game_location);
+		EditText date = (EditText) findViewById(R.id.game_date);
+		
+		// checks to see if location and text are empty
+		return location.getText().toString().trim().isEmpty() &&
+				date.getText().toString().trim().isEmpty();
 	}
 
 }
