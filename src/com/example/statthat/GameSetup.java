1d5611721dc0b7b02992eebc90f570b446e70315
Andrew Dorsett <awdorsett@berkeley.edu>
2014-04-05 17:14:22 -0700

    added in getting team id

--- src/com/example/statthat/GameSetup.java
+++ src/com/example/statthat/GameSetup.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -17,24 +18,38 @@ import android.widget.DatePicker;
 public class GameSetup extends Activity {
 	private String error =""; 
 	private EditText date;
-
+	private EditText location;
+	private Team team = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_setup);
 		
-		date = (EditText) findViewById(R.id.game_date);
+		// Get team passed from intent
+		Bundle extras = getIntent().getExtras();
+		if(extras != null){
+			Long team_id = extras.getLong("team");
+			team = Team.findById(Team.class, team_id);
+		}else{
+			// for testing
+			// TODO delete for production
+			team = Team.findById(Team.class, (long)1);
+		}
 		
+		
+		
+		
+		
+		// Date dialog and selection
+		date = (EditText) findViewById(R.id.game_date);
 		Button datePicker = (Button) findViewById(R.id.date_picker);
 		
 		datePicker.setOnClickListener(new Button.OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
-				showDialog(1111);
+				showDialog(1);
 			}
-			
-			
 		});
 	
 		
@@ -45,8 +60,11 @@ public class GameSetup extends Activity {
 			@Override
 			public void onClick(View v) {
 				if(validateFields()){
-					
+					location = (EditText) findViewById(R.id.game_location);
+					Game game = new Game(getApplicationContext(), team, "Oops", location.getText().toString(), date.getText().toString());
+					game.save();
 				}else{
+					// Error message if fields are not filled in
 					int duration = Toast.LENGTH_SHORT;
 
 					Toast toast = Toast.makeText(getApplicationContext(), error, duration);
@@ -80,10 +98,11 @@ public class GameSetup extends Activity {
 	}
 	
 	
+	// Date selector code
 	@Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
-        case 1111:
+        case 1:
              
             // open datepicker dialog. 
             // set date picker for current date 
