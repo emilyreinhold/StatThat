39441392e73477d5d01ada91a5033ba356efc482
Daphne Hsu <daphnehsu@Daphnes-MacBook-Pro.local>
2014-05-03 15:05:57 -0700

    cleanup

--- src/com/example/statthat/GameActivity.java
+++ src/com/example/statthat/GameActivity.java
@@ -32,7 +32,6 @@ import android.widget.TextView;
 
 
 public class GameActivity extends FragmentActivity {
-	MyPageAdapter pageAdapter;
 	String opposingTeamName;
 	static String location;
 	static String teamName;
@@ -48,60 +47,13 @@ public class GameActivity extends FragmentActivity {
 		TextView tView = (TextView) findViewById(R.id.team_title);
 		tView.setText(teamName);
 
-
-		Button b = (Button) findViewById(R.id.button_team);
-		b.setPressed(true);
-
 		getActionBar().hide();
 		getActionBar().setDisplayShowHomeEnabled(false);              
 		getActionBar().setDisplayShowTitleEnabled(false);
-		List<Fragment> fragments = getFragments();
-		pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
-		final ViewPager pager = (ViewPager)findViewById(R.id.game_pager);
-		pager.setAdapter(pageAdapter);
 
-		final ActionBar actionBar = getActionBar();
-
-		// Specify that tabs should be displayed in the action bar.
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-		// Create a tab listener that is called when the user changes tabs.
-		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-			public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-				// show the given tab
-				pager.setCurrentItem(tab.getPosition());
-			}
-
-			public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-				// hide the given tab
-			}
-
-			public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-				// probably ignore this event
-			}
-		};
-
-
-		actionBar.addTab(actionBar.newTab().setText("Team").setTabListener(tabListener));
-		actionBar.addTab(actionBar.newTab().setText("Players").setTabListener(tabListener));
-
-
-		pager.setOnPageChangeListener(
-				new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						// When swiping between pages, select the
-						// corresponding tab.
-						getActionBar().setSelectedNavigationItem(position);
-					}
-				});
-
-
-
+		
 		FragmentTabHost tabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
 		tabHost.setup(this, getSupportFragmentManager(), R.id.tab_1);
-
-
 
 		tabHost.addTab(tabHost.newTabSpec("team").setIndicator("Team Stats"),
 				TFragment.class, null);
@@ -113,7 +65,6 @@ public class GameActivity extends FragmentActivity {
 
 
 	public static class TFragment extends Fragment {
-
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -275,227 +226,8 @@ public class GameActivity extends FragmentActivity {
 	}
 
 
-	// when pressing back button, we still want the button to be highlighted
-	//    protected void onResume() {
-	//    	super.onResume();
-	//    	Button b = (Button) findViewById(R.id.button_team);
-	//		b.setPressed(true);
-	//    }
 
 
-	// FOR THE BUTTONS
-
-
-	public void onPlayersClick(View v) {
-		Intent intent = new Intent(GameActivity.this, GamePlayersActivity.class);
-		intent.putExtra("location", location);
-		intent.putExtra("teamName", teamName);
-		GameActivity.this.startActivity(intent);
-
-	}
-
-	HashMap<String, HashMap<String, Integer>> players;
-
-	private List<Fragment> getFragments(){
-		List<Fragment> fList = new ArrayList<Fragment>();
-		players = new HashMap<String, HashMap<String, Integer>>();
-
-		List<Game> games = Game.find(Game.class, "location = ?", location);
-		Game game = games.get(0);
-
-		Team t = Team.find(Team.class, "name = ?", teamName).get(0);
-		List<Player> plays = t.getPlayers();
-
-		for (Player p: plays) {
-			HashMap<String, Integer> h = new HashMap<String, Integer>();
-			List<Stat> playerStats = game.getStatsForPlayer(p); // questionable
-			for(Stat s: playerStats) {
-				String stat = s.statType.name;
-				if (h.containsKey(s)) {
-					int c = h.get(s);
-					c ++;
-					h.put(stat, c);
-				}
-				else {
-					h.put(stat, 1);
-				}
-			}
-			players.put(p.firstName + " " + p.lastName, h);
-		}
-
-		// make team stats
-		HashMap<String, Integer> team = new HashMap<String,Integer>();
-		ArrayList<Integer> statCounts = t.getBballStatCountsForGame(game);
-		ArrayList<String> statNames = StatType.getBballStatTypeNames();
-		for (int i = 0; i < statCounts.size(); i++) {
-			team.put(statNames.get(i), statCounts.get(i));
-		}
-		//		team.put("Points Scored", "56");
-		//		team.put("Rebounds", "20");
-		//		team.put("Fouls", "9");
-		//		team.put("Three Pointers", "11");		
-		//		team.put("Layups", "15");
-		//		team.put("Time-outs", "3");
-		//		team.put("Free Throws", "20");
-		//		team.put("Assist", "40");
-		//		team.put("Steal", "16");
-		//		team.put("Block", "20");
-		//		team.put("Turnover", "5");
-
-		fList.add(MyFragment.newInstance(team));		
-		fList.add(MyFragment.newInstance(players, 0)); 
-
-		return fList;
-	}
-
-	public static class MyFragment extends Fragment implements OnClickListener, OnItemSelectedListener{
-		public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
-		public static final String PLAYERS = "PLAYERS";
-		public static final String TEAM = "TEAM";
-
-		View v;
-		HashMap<String, HashMap<String,String>> hash;
-
-		// this is for players with stats
-		// hacky way to differentiate between team and players
-		public final static MyFragment newInstance(HashMap<String,HashMap<String,Integer>> message, int x) {
-			MyFragment f = new MyFragment();
-			Bundle bdl = new Bundle(message.size());
-			bdl.putSerializable(PLAYERS, message);
-			f.setArguments(bdl);
-			return f;
-		}
-
-		// for team
-		public final static MyFragment newInstance(HashMap<String,?> message) {
-			MyFragment f = new MyFragment();
-			Bundle bdl = new Bundle(message.size());
-			bdl.putSerializable(TEAM, message);
-			f.setArguments(bdl);
-			return f;
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			v = inflater.inflate(R.layout.myfragment_game_layout, container, false);
-
-			if (getArguments().getSerializable(PLAYERS) instanceof HashMap<?,?>) {
-				v = inflater.inflate(R.layout.myfragment_game_viewplayers, container, false);
-
-				hash = (HashMap<String, HashMap<String,String>>) getArguments().getSerializable(PLAYERS);
-				LinearLayout gamesLayout = (LinearLayout)v.findViewById(R.id.linear_players); 
-				Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
-
-				List l = new ArrayList();
-				l.addAll(hash.keySet());
-				ArrayAdapter<String> adapter=new ArrayAdapter<String>(v.getContext(),
-						android.R.layout.simple_dropdown_item_1line, l);
-
-				adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-				spinner.setAdapter(adapter);
-				spinner.setOnItemSelectedListener(this);
-			}
-
-			if (getArguments().getSerializable(TEAM) instanceof HashMap<?,?>) {
-
-				v = inflater.inflate(R.layout.myfragment_game_viewteam, container, false);
-				HashMap<String, String> hash = (HashMap<String, String>) getArguments().getSerializable(TEAM);
-				TableLayout layout = (TableLayout) v.findViewById(R.id.table_layout_team);
-
-				// reset tablerows
-				layout.removeAllViews();
-
-				for (String stat: StatType.getBballStatTypeNames()) {
-					if (!hash.containsKey(stat)) {
-						continue;
-					}
-					TableRow row = new TableRow(v.getContext());
-					TextView s = new TextView(v.getContext());
-
-					s.setText(stat + "        ");
-					s.setTextSize(30);
-
-					TextView ss = new TextView(v.getContext());
-					ss.setText(String.valueOf(hash.get(stat)));
-					ss.setTextSize(30);
-
-					row.addView(s);
-					row.addView(ss);
-					layout.addView(row);			
-				}
-			}
-			return v;
-		}
-
-
-		@Override
-		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-
-
-		}
-
-		/*
-		 * For spinner
-		 * 
-		 */
-		@Override
-		public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-				long arg3) {
-			// TODO Auto-generated method stub
-			// make spinner text bigger
-			((TextView) arg0.getChildAt(0)).setTextSize(25);			
-			String s = (String) arg0.getItemAtPosition(arg2);
-			TableLayout layout = (TableLayout) v.findViewById(R.id.table_layout);
-			// reset tablerows
-			layout.removeAllViews();
-			for (String stat: StatType.getBballStatTypeNames()) {
-				if (!hash.get(s).containsKey(stat)) {
-					continue;
-				}
-				TableRow row = new TableRow(v.getContext());
-				TextView t = new TextView(v.getContext());
-				t.setText(stat + "        ");
-				t.setTextSize(30);
-
-				TextView tt = new TextView(v.getContext());
-				tt.setText(String.valueOf(hash.get(s).get(stat)));
-				tt.setTextSize(30);
-
-				row.addView(t);
-				row.addView(tt);
-				layout.addView(row);		
-			}
-
-		}
-
-		@Override
-		public void onNothingSelected(AdapterView<?> arg0) {
-			// TODO Auto-generated method stub
-
-		}
-	}
-
-
-	class MyPageAdapter extends FragmentPagerAdapter {
-		private List<Fragment> fragments;
-
-		public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
-			super(fm);
-			this.fragments = fragments;
-		}
-		@Override 
-		public Fragment getItem(int position) {
-			return this.fragments.get(position);
-		}
-
-		@Override
-		public int getCount() {
-			return this.fragments.size();
-		}
-	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- src/com/example/statthat/TeamPageActivity.java
+++ src/com/example/statthat/TeamPageActivity.java
@@ -37,7 +37,6 @@ import android.widget.TextView;
 
 @SuppressWarnings("deprecation")
 public class TeamPageActivity extends FragmentActivity { // fragmentactivity
-	MyPageAdapter pageAdapter;
 	public static String teamName;
 	// http://architects.dzone.com/articles/android-tutorial-using
 	@SuppressLint("NewApi")
@@ -56,46 +55,6 @@ public class TeamPageActivity extends FragmentActivity { // fragmentactivity
 
 		TextView tView = (TextView) findViewById(R.id.team_title2);
 		tView.setText(teamName);
-
-		List<Fragment> fragments = getFragments();
-		pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
-		final ViewPager pager = (ViewPager)findViewById(R.id.pager);
-		pager.setAdapter(pageAdapter);
-		final ActionBar actionBar = getActionBar();
-
-		// Specify that tabs should be displayed in the action bar.
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-		// Create a tab listener that is called when the user changes tabs.
-		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-			public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-				// show the given tab
-				pager.setCurrentItem(tab.getPosition());
-			}
-
-			public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-				// hide the given tab
-			}
-
-			public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-				// probably ignore this event
-			}
-		};
-
-		actionBar.addTab(actionBar.newTab().setText("Players").setTabListener(tabListener));
-		actionBar.addTab(actionBar.newTab().setText("Stats").setTabListener(tabListener));
-
-		pager.setOnPageChangeListener(
-				new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						// When swiping between pages, select the
-						// corresponding tab.
-						getActionBar().setSelectedNavigationItem(position);
-					}
-				});
-
-
 
 		FragmentTabHost tabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
 		tabHost.setup(this, getSupportFragmentManager(), R.id.tab_1);
@@ -156,23 +115,6 @@ public class TeamPageActivity extends FragmentActivity { // fragmentactivity
 			tableLayout.addView(r);
 
 			for (int i1=0; i1 < playerNameThenNumber.length; i1 += 2) {
-
-//				TableRow row = new TableRow(v.getContext());
-//				TextView s = new TextView(v.getContext());
-//
-//
-//				s.setText(playerNameThenNumber[i1] + "       "); //hacky way to add space
-//				s.setTextSize(20);
-//
-//				TextView ss = new TextView(v.getContext());
-//				ss.setText(playerNameThenNumber[i1+1]);
-//				ss.setTextSize(20);
-//
-//				row.addView(s);
-//				row.addView(ss);
-//				
-//				tableLayout.addView(row);
-				
 				
 				TableRow row = new TableRow(v.getContext());
 				TextView s = new TextView(v.getContext());
@@ -198,73 +140,6 @@ public class TeamPageActivity extends FragmentActivity { // fragmentactivity
 				tableLayout.addView(row);
 				tableLayout.addView(row2);		
 				
-			
-				
-				
-//				ShapeDrawable border = new ShapeDrawable(new RectShape());
-//				
-//				border.getPaint().setStyle(Style.STROKE);
-//				border.getPaint().setColor(Color.BLACK);
-//				
-//				
-//				
-//				TableRow row = new TableRow(v.getContext());
-//				TextView s = new TextView(v.getContext());
-//
-//
-//				s.setText(playerNameThenNumber[i1] + "       "); //hacky way to add space
-//				s.setPadding(8, 5, 8, 5);
-//				s.setTextSize(20);
-//				
-//
-//				TextView ss = new TextView(v.getContext());
-//				ss.setPadding(8, 5, 8, 5);
-//				ss.setText(playerNameThenNumber[i1+1]);
-//				ss.setTextSize(20);
-//				
-//				s.setBackground(border);
-//				ss.setBackground(border);
-//
-//				row.addView(s);
-//				row.addView(ss);
-//		
-//				//row.setBackground(border);
-//				
-//				tableLayout.addView(row);
-				
-				
-				
-				
-				
-				
-				
-//				TableRow tr = new TableRow(v.getContext());
-//				tr.setBackgroundColor(Color.BLACK);
-//				tr.setPadding(0, 0, 0, 2); //Border between rows
-//			
-//				
-//				TableRow.LayoutParams llp = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-//				//llp.setMargins(0, 0, 2, 0);//2px right-margin
-//
-//				//New Cell
-//				LinearLayout cell = new LinearLayout(v.getContext());
-//				cell.setBackgroundColor(Color.WHITE);
-//				cell.setLayoutParams(llp);//2px border on the right for the cell
-//
-//
-//				TextView tv = new TextView(v.getContext());
-//				tv.setText("Some Text");
-//				tv.setPadding(0, 0, 4, 3);
-//
-//				cell.addView(tv);
-//				tr.addView(cell);
-//				//add as many cells you want to a row, using the same approach
-//
-//				tableLayout.addView(tr);
-				
-				
-				
-				
 				
 
 			}
@@ -275,12 +150,6 @@ public class TeamPageActivity extends FragmentActivity { // fragmentactivity
 	}
 
 
-	// when pressing back button, we still want the button to be highlighted
-	//	protected void onResume() {
-	//		super.onResume();
-	//		Button b = (Button) findViewById(R.id.button_team_page);
-	//		b.setPressed(true);
-	//	}
 
 	// FOR BUTTON
 	public void onStatsClick(View v) {
@@ -341,198 +210,6 @@ public class TeamPageActivity extends FragmentActivity { // fragmentactivity
 			intent.putExtra("location", bArr[3]);
 			startActivity(intent);
 
-		}
-	}
-
-	private List<Fragment> getFragments(){
-		List<Fragment> fList = new ArrayList<Fragment>();
-
-		// instead of hard coding, get players from Players class; will do later
-
-		List<Team> t = Team.find(Team.class, "name = ?", teamName);
-		Team team = t.get(0);
-		List<Player> p = team.getPlayers();
-
-
-		fList.add(MyFragment.newInstance(p));
-
-		List<Game> games = team.getGames();
-		HashMap<String, String> gamess = new HashMap<String, String>();
-
-		for (Game g : games) {
-			gamess.put("vs. " + g.opposingTeamName, g.date + " @ " + g.location);
-		}
-
-		fList.add(MyFragment.newInstance(gamess)); 
-
-		return fList;
-	}
-
-	public static class MyFragment extends Fragment implements OnClickListener{
-		public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
-
-		public final static MyFragment newInstance(String message)
-		{
-			MyFragment f = new MyFragment();
-			Bundle bdl = new Bundle(1);
-			bdl.putString(EXTRA_MESSAGE, message);
-			f.setArguments(bdl);
-			return f;
-		}
-
-		public final static MyFragment newInstance(HashMap<String,String> message) {
-			MyFragment f = new MyFragment();
-			Bundle bdl = new Bundle(message.size());
-
-			bdl.putSerializable(EXTRA_MESSAGE, message);
-			f.setArguments(bdl);
-			return f;
-		}
-
-
-		public final static MyFragment newInstance(String[] message)
-		{
-			MyFragment f = new MyFragment();
-			Bundle bdl = new Bundle(message.length);
-			bdl.putStringArray(EXTRA_MESSAGE, message);
-			f.setArguments(bdl);
-			return f;
-		}
-
-		public final static MyFragment newInstance(List<Player> message)
-		{
-			MyFragment f = new MyFragment();
-			Bundle bdl = new Bundle(message.size());
-			String[] playerNameThenNumber = new String[message.size() * 2];
-
-			int i = 0;
-			for (Player p: message) {
-				String name = p.firstName + " " + p.lastName;
-				String number = Integer.toString(p.number);
-				playerNameThenNumber[i] = name; 
-				playerNameThenNumber[ i + 1] = number; 
-				i += 2;
-
-			}
-
-			bdl.putStringArray(EXTRA_MESSAGE, playerNameThenNumber);
-			f.setArguments(bdl);
-			return f;
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View v = inflater.inflate(R.layout.myfragment_layout, container, false);
-
-			if (getArguments().getString(EXTRA_MESSAGE) instanceof String) {
-				String message = getArguments().getString(EXTRA_MESSAGE);
-				TextView messageTextView = (TextView)v.findViewById(R.id.textView);
-				messageTextView.setText(message);
-
-			}
-
-			// for player and number
-			if (getArguments().getStringArray(EXTRA_MESSAGE) instanceof String[]) {
-				String[] message = getArguments().getStringArray(EXTRA_MESSAGE);
-
-				TableLayout tableLayout = (TableLayout) v.findViewById(R.id.table_layout);
-
-				// reset tablerows
-				tableLayout.removeAllViews();
-
-				TableRow r = new TableRow(v.getContext());
-				r.setPadding(0, 0, 0, 18);
-				TextView s1 = new TextView(v.getContext());
-				TextView s2 = new TextView(v.getContext());
-				s1.setText("Player");
-				s1.setTextSize(23);
-				s2.setText("#");
-				s2.setTextSize(23);
-				r.addView(s1);
-				r.addView(s2);
-
-				// add line
-				//				View view = new View(v.getContext());
-				//				view.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, 1));
-				//				view.setBackgroundColor(Color.rgb(51, 51, 51));
-
-				tableLayout.addView(r);
-				//tableLayout.addView(view);
-
-				for (int i=0; i < message.length; i += 2) {
-
-					TableRow row = new TableRow(v.getContext());
-					TextView s = new TextView(v.getContext());
-
-
-					s.setText(message[i] + "       "); //hacky way to add space
-					s.setTextSize(20);
-
-					TextView ss = new TextView(v.getContext());
-					ss.setText(message[i+1]);
-					ss.setTextSize(20);
-
-					row.addView(s);
-					row.addView(ss);
-					tableLayout.addView(row);	
-				}
-			}
-
-			if (getArguments().getSerializable(EXTRA_MESSAGE) instanceof HashMap<?,?>) {
-
-				HashMap<String, String> hash = (HashMap<String, String>) getArguments().getSerializable(EXTRA_MESSAGE);
-				LinearLayout gamesLayout = (LinearLayout)v.findViewById(R.id.linear_games);
-
-				for (String vs : hash.keySet()) {
-					Button button = new Button(container.getContext());
-					button.setText(vs + "\n" + hash.get(vs));
-					button.setOnClickListener(this);
-					button.setTextSize(25);
-					LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-					params.setMargins(10,10,10,10);
-					button.setBackgroundResource(R.drawable.custom_button_confirm);
-					button.setLayoutParams(params);
-					gamesLayout.addView(button);
-				}
-			}
-			return v;
-		}
-
-
-		@Override
-		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-
-			// intent to game page
-			Button b = (Button) arg0;
-			String bText = b.getText().toString();
-			String[] bArr = bText.split(" ");
-
-			Intent intent = new Intent(arg0.getContext(), GameActivity.class);
-			intent.putExtra("teamName", teamName);
-			intent.putExtra("location", bArr[3]);
-			startActivity(intent);
-
-		}
-	}
-
-
-	class MyPageAdapter extends FragmentPagerAdapter {
-		private List<Fragment> fragments;
-
-		public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
-			super(fm);
-			this.fragments = fragments;
-		}
-		@Override 
-		public Fragment getItem(int position) {
-			return this.fragments.get(position);
-		}
-
-		@Override
-		public int getCount() {
-			return this.fragments.size();
 		}
 	}
 
